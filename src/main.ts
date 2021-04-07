4755ca0b2ee853ace6709e9a062512858d1dc613
denini08 <denini.gabriel@gmail.com>
2021-04-07 13:59:03 -0300

    l

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import fs from 'fs';
 
 
 async function run() {
-  const path_src = "/System/Volumes/Data/Users/runner/work/_actions/denini08/shaker-action/v0.0.2.33/src/"
+  const path_src = "/System/Volumes/Data/Users/runner/work/_actions/denini08/shaker-action/v0.0.2.32/src/"
   try {
     // only support running on macOS or Linux
     if (process.platform !== 'darwin') {
